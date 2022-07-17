dfa6bc68dfe44a17d851ca042969ce68eba24562
ahmedkazim <ahmedkazim.ak@gmail.com>
2022-07-17 22:57:58 +0500

    Add Xiaomi Parts and Doze

--- common.mk
+++ common.mk
@@ -103,6 +103,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.xiaomiparts.rc \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.lcd.cabc_mode=1
+
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-impl \
